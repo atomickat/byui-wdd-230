439e68c2b62329efe74e97ce0e5db9f7e45f1ea7
atomickat <cha13079@byui.edu>
2021-09-30 21:28:13 -0500

    fixes 9-30

--- lesson3/js/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("copyrightyear").textContent = new Date().getFullYear();
-
-const lastDateModif = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour12: true, hour:'numeric', minute:'numeric' };
-
-document.getElementById("lastupdated").textContent = new Date().toLocaleDateString('en-US', lastDateModif);
