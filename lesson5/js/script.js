d7aab8733ba891f4cd7f41e99f729d8b8848bfaa
atomickat <cha13079@byui.edu>
2021-10-25 16:55:42 -0500

    start of week 7

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -5,13 +5,21 @@ const currentyear = { weekday: 'long', day: 'numeric', month: 'long', year: 'num
 
 document.getElementById("currentyear").textContent = new Date().toLocaleDateString('en-US', currentyear);
 
-
 const menubutton = document.querySelector('.menu');
 const mainnav = document.querySelector('.navigation')
 
 menubutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
 
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+window.onresize = () => { if (window.innerWidth > 760) mainnav.classList.remove('responsive') };
+
+function toggleBanner() {
+  document.querySelector('.navigation').classList.toggle('responsive');
+}
+
+const days = new Date();
+if (days.getDate() == 1) {
+  document.querySelector('.pancakes').style.display = 'block';
+}
 
 // const banner = document.querySelector('.cooking');
 // const dayOfWeek = 
--- lesson6/js/script.js
+++ lesson6/js/script.js
@@ -5,16 +5,25 @@ const currentyear = { weekday: 'long', day: 'numeric', month: 'long', year: 'num
 
 document.getElementById("currentyear").textContent = new Date().toLocaleDateString('en-US', currentyear);
 
-
 const menubutton = document.querySelector('.menu');
 const mainnav = document.querySelector('.navigation')
 
 menubutton.addEventListener('click', () => { mainnav.classList.toggle('responsive') }, false);
 
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+window.onresize = () => { if (window.innerWidth > 760) mainnav.classList.remove('responsive') };
+
+function toggleBanner() {
+  document.querySelector('.navigation').classList.toggle('responsive');
+}
+
+const days = new Date();
+if (days.getDate() == 1) {
+  document.querySelector('.pancakes').style.display = 'block';
+}
 
 // const banner = document.querySelector('.cooking');
+// const dayOfWeek = 
 // const banner = document.querySelector('.pancakes');
-// banner.addEventListener('load',('banner') => {if (dayOfWeek == 6)});
+// banner.addEventListener('load',('pancakes') => {if (dayOfWeek == 4)});
 
 
