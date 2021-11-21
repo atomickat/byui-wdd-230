928c3de29158a022024683d7d77f2928999ac4bf
atomickat <cha13079@byui.edu>
2021-11-20 22:28:30 -0600

    week 10 done

--- lesson10/js/five-day-forecast.js
+++ lesson10/js/five-day-forecast.js
@@ -1,41 +1,34 @@
 const fiveDay = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=a738ef502266a0b1d2632c134155ebb1";
 
+
 fetch(fiveDay)
-  .then((response) => response.json())
-  .then((jsObject) => {
+  .then(response => response.json())
+  .then(jsObject => {
     console.log(jsObject);
 
-    // let currentCondition = document.querySelector('#current');
-    // currentCondition.textContent = jsObject.weather[0].main;
-
-    let tempHigh1 = document.querySelector('#high');
-    tempHigh1.textContent = jsObject.list[6].main.temp_max.toFixed(0);
-
-    // let tempLow = document.querySelector('#low');
-    // tempLow.textContent = jsObject.main.temp_min.toFixed(0);
-
-    // let humidity = document.querySelector('#humid');
-    // humidity.textContent = jsObject.main.humidity;
-
-    // let speed = document.querySelector('#wind-speed');
-    // speed.textContent = jsObject.wind.speed.toFixed(0);
     
-    // const t = jsObject.main.temp.toFixed(0);
-    // const s = jsObject.wind.speed;
-    // let answer = windChill(t, s);
+    const sixOclock = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+    console.log(sixOclock);
 
-    // document.querySelector('#wind-chill').textContent = answer.toFixed(0);
+    const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    let day = 0;
 
-    // function windChill(tempF, speed) {
-    //   let answer = 35.74 + 0.6215 * tempF - 35.75 * (speed ** 0.16) + 0.4275 * tempF * (speed ** 0.16);
-
-    //   return answer;
-    // }
     
-   
+      
+    sixOclock.forEach(forecast => {
+      let theDate = new Date(forecast.dt_txt);
+      
+      // console.log(theDate);
+      document.querySelector(`#day${day + 1}`).textContent = weekdays[theDate.getDay()];
+      document.querySelector(`#high${day + 1}`).textContent = forecast.main.temp_max.toFixed(0);
+      document.querySelector(`#low${day + 1}`).textContent = forecast.main.temp_min.toFixed(0);
+  
+      day++;
+     
+    });
 
+ 
+  
   });
 
-  
-  // ------- days of the week rotate in five day forecast
-  // -------- out put day of the week per from the date
+
--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,72 +1,72 @@
 
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+// const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
 
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
+// fetch(requestURL)
+//   .then(function (response) {
+//     return response.json();
+//   })
   
-  .then(function (jsonObject) {
+//   .then(function (jsonObject) {
   
     // temporary checking for valid response and data parsing
     // console.table(jsonObject);
 
-      const prophets = jsonObject['prophets'];
+    //   const prophets = jsonObject['prophets'];
     
-    for (let i = 0; i < prophets.length; i++) {
+    // for (let i = 0; i < prophets.length; i++) {
 
-      const container = document.querySelector('.cards');
-      let card = document.createElement('section');
+    //   const container = document.querySelector('.cards');
+    //   let card = document.createElement('section');
 
-      let h2 = document.createElement('h2');
+    //   let h2 = document.createElement('h2');
       // h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-      h2.textContent = `${prophets[i].name} ${prophets[i].lastname}`;
+      // h2.textContent = `${prophets[i].name} ${prophets[i].lastname}`;
 
-      let dob = document.createElement('p');
+      // let dob = document.createElement('p');
 
-      dob.textContent = `Birthdate: ${prophets[i].birthdate}`;
+      // dob.textContent = `Birthdate: ${prophets[i].birthdate}`;
       // dob.textContent = prophets[i].birthdate;
 
-      let bp = document.createElement('p');
-      bp.textContent = `Brirthplace: ${ prophets[i].birthplace}`;
+  //     let bp = document.createElement('p');
+  //     bp.textContent = `Brirthplace: ${ prophets[i].birthplace}`;
 
-      let image = document.createElement('img');
-      image.setAttribute('src', prophets[i].imageurl);
+  //     let image = document.createElement('img');
+  //     image.setAttribute('src', prophets[i].imageurl);
 
-      container.appendChild(card)
-      card.appendChild(h2);
-      card.appendChild(dob);
-      card.appendChild(bp);
-      card.appendChild(image);
+  //     container.appendChild(card)
+  //     card.appendChild(h2);
+  //     card.appendChild(dob);
+  //     card.appendChild(bp);
+  //     card.appendChild(image);
 
-      document.querySelector('div.cards').appendChild(card);
+  //     document.querySelector('div.cards').appendChild(card);
       
-    }
-  });
+  //   }
+  // });
 
 // ----- another way to code jason -----
 
-// const URL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const URL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
-// fetch(URL)
-//   .then(response => {
-//     return response.json();
-//   }).then(jasonObject => {
-//     console.log(jasonObject);
-//     const prophets = jasonObject['prophets'];
-//     const main = document.querySelector('main');
-//     for (let i = 0; i < prophets.length; i++) {
-//       const section = document.createElement('section');
-//       section.innerHTML = `<h1>${prophets[i].name} ${prophets[i].lastname}<h1>
-//       <img src="${prophets[i].imageurl}" alt="${prophets[i].name} - ${prophets[i].order}"/>
-//       <p>
-//         Date: ${prophets[i].birthdate} <br>
-//         Brithplace: ${prophets[i].birthplace}
-//       </p>`
-//       main.appendChild(section);
-//     }
-//   })
+fetch(URL)
+  .then(response => {
+    return response.json();
+  }).then(jasonObject => {
+    console.log(jasonObject);
+    const prophets = jasonObject['prophets'];
+    const main = document.querySelector('main');
+    for (let i = 0; i < prophets.length; i++) {
+      const section = document.createElement('section');
+      section.innerHTML = `<h1>${prophets[i].name} ${prophets[i].lastname}<h1>
+      <img src="${prophets[i].imageurl}" alt="${prophets[i].name} - ${prophets[i].order}"/>
+      <p>
+        Date: ${prophets[i].birthdate} <br>
+        Brithplace: ${prophets[i].birthplace}
+      </p>`
+      main.appendChild(section);
+    }
+  })
 
 
   
